a025e056ec7a7724420e15807d9b2d9d409989f2
lsq210 <1135595767@qq.com>
2018-03-21 04:16:46 +0800

    搭建 WebPack + Vue 2.0 系统框架

---
+++ src/components/ArcGIS/map.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div id="viewDiv"></div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import * as esriLoader from 'esri-loader'
+
+esriLoader.loadModules(['esri/views/MapView', 'esri/WebMap'])
+.then(([MapView, WebMap]) => {
+  // then we load a web map from an id
+  let webmap = new WebMap({
+    portalItem: {id: 'f2e9b762544945f390ca4ac3671cfa72'}
+    })
+    let view = new MapView({
+    map: webmap,
+    container: 'viewDiv'
+  });
+})
+.catch(err => {
+  // handle any errors
+  console.error(err);
+});
+
+export default {
+}
+</script>
+<style scoped>
+  @import url('https://js.arcgis.com/4.4/esri/themes/light/main.css');
+  #viewDiv {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+</style>
